416f6dff15492295cd1d02ba755c7e0ba6908b5b
Swarit Pandey <swarit@accuknox.com>
2024-10-01 11:08:53 +0530

    fix: some bindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>find', builtin.find_files, {}) -- fuzzy find the files
-vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- include git
-vim.keymap.set('n', '<leader>buf', builtin.buffers, {})
-vim.keymap.set('n', '<leader>rip', function()
-	builtin.grep_string({ search = vim.fn.input("RIP > ") }); -- Grepit!
+vim.keymap.set('n', '<leader>l', builtin.find_files, {}) -- fuzzy find the files
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})      -- include git
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>z', function()
+    builtin.grep_string({ search = vim.fn.input("RIP > ") }); -- Grepit!
 end)
--- lua/gems/remap.lua
+++ lua/gems/remap.lua
@@ -6,16 +6,9 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz") 
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
